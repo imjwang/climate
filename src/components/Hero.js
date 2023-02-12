08adbded291a8004358b03884449d0780782b838
Jeffrey Wang <ngjaywa@gmail.com>
2023-02-12 17:17:25 -0500

    hero

---
+++ src/components/Hero.js
@@ -0,0 +1,41 @@
+import {Container, Box, Typography, Button} from '@mui/material';
+import HeroLayout from './HeroLayout';
+
+const Hero = () => {
+  return (
+    <HeroLayout
+      sxBackground={{
+        backgroundImage: `url("/background.jpeg")`,
+        backgroundColor: '#cyan',
+        backgroundPosition: 'center',
+      }}
+    >
+      <Typography color="inherit" align="center" variant="h2" marked="center">
+        Upgrade your Sundays
+      </Typography>
+      <Typography
+        color="inherit"
+        align="center"
+        variant="h5"
+        sx={{ mb: 4, mt: { xs: 4, sm: 10 } }}
+      >
+        Enjoy secret offers up to -70% off the best luxury hotels every Sunday.
+      </Typography>
+      <Button
+        color="secondary"
+        variant="contained"
+        size="large"
+        component="a"
+        href="/premium-themes/onepirate/sign-up/"
+        sx={{ minWidth: 200 }}
+      >
+        Register
+      </Button>
+      <Typography variant="body2" color="inherit" sx={{ mt: 2 }}>
+        Discover the experience
+      </Typography>
+    </HeroLayout>
+  );
+}
+
+export default Hero;
---
+++ src/utils/firebase.js
@@ -0,0 +1,37 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { getFirestore, collection, limit, doc, getDocs, query, where } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDgNeFTYqQgH673x-ue8Nm1ZC2aR8o0v4w",
+  authDomain: "climate-823dd.firebaseapp.com",
+  projectId: "climate-823dd",
+  storageBucket: "climate-823dd.appspot.com",
+  messagingSenderId: "118409926388",
+  appId: "1:118409926388:web:5eb707f6700453e3e6f0a1",
+  measurementId: "G-N0NPVKCP81"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const firestore = getFirestore(app);
+export const storage = getStorage(app);
+
+//Example
+export async function getUserWithUsername(username) {
+  const usersRef = collection(firestore, 'users');
+  const q = query(usersRef, where('username', '==', username), limit(1));
+  const querySnapshot = await getDocs(q);
+  const userDoc = querySnapshot.docs[0];
+  return userDoc;
+}
